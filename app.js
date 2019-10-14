8148f01182e285409d239d8a19bb617a54e9df4c
X-yn <ryftcore@gmail.com>
2019-10-14 19:16:03 +0530

    added mongodb

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 4000;
+
+mongoose.connect(
+  'mongodb+srv://admin:5bB3sURW35uMVsOb@graphql-project-cluster-quy2e.mongodb.net/test?retryWrites=true&w=majority'
+);
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.use(
   '/graphql',
