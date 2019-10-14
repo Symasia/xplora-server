7749988f015966830a4bd1862efd321581e668f9
X-yn <ryftcore@gmail.com>
2019-10-14 19:22:14 +0530

    added author and book models

---
+++ models/book.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+const bookSchema = new Schema({
+  name: String,
+  genre: String,
+  authorId: String
+});
+
+module.exports = model('Book', bookSchema);
